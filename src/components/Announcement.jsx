117ce50839872e1642dddd3b523fc620d849dd89
sorinupwork <sorin.upwork@gmail.com>
2023-07-10 02:58:33 +0300

    add navbar

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Announcement = () => {
+  return (
+    <div className="h-7 bg-teal-600 text-white flex items-center justify-center text-md font-bold">
+      Super Deal! Free Shipping on Orders Over $50
+    </div>
+  );
+};
+
+export default Announcement;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import Badge from "@mui/material/Badge";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+
+const Navbar = () => {
+  return (
+    <div className="h-16">
+      <div className="flex justify-between px-5 py-2.5">
+        <div className="flex flex-1 items-center">
+          <span className="cursor-pointer text-sm">EN </span>
+          <div className="border border-solid border-gray-300 flex items-center ml-6 p-1">
+            <input className="outline-none" type="text" placeholder="search" />
+            <SearchIcon className="text-gray-600 text-md cursor-pointer" />
+          </div>
+        </div>
+        <div className="flex-1">
+          <h1 className="font-bold text-3xl text-center">Madam Ink Online</h1>
+        </div>
+        <div className=" flex flex-1 items-center justify-end">
+          <div className="text-sm cursor-pointer ml-6">REGISTER</div>
+          <div className="text-sm cursor-pointer ml-6">SIGN IN</div>
+          <div className="text-sm cursor-pointer ml-6">
+            <Badge badgeContent={3} color="primary">
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
